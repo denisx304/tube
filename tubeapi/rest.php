10205e4f421f6888abccf5bc853396df31a25bd1
HoriaCr <cretescu_horia@yahoo.com>
2015-05-19 23:29:22 +0300

    Added doc

--- tubeapi/rest.php
+++ tubeapi/rest.php
@@ -1,27 +1,43 @@
 <?php
     class Rest {
         
-        public $_allow = array();
-        public $_content_type = "application/json";
-        public $_request = array();
+        protected $_allow;
+        protected $_content_type = "application/json";
+        protected $_request;
         
         private $_method = "";        
         private $_code = 200;
         
         public function __construct() {
+            $this->allow = array();
+            $this->request = array();
             $this->inputs();
         }
-        
-        public function getReferer() {
-            return $_SERVER['HTTP_REFERER'];
-        }
-        
-        public function response($data, $status) {
+
+        /*  Respond method.
+         *
+         *  Respond with the given arguments
+         *  and exit.
+         *
+         *  @param array data
+         *  @param int status
+         *
+         *  @return void
+         */
+
+        protected function response($data, $status) {
             $this->_code = ($status) ? $status : 200;
             $this->setHeaders();
             echo $data;
             exit;
         }
+
+        /*  Get status message.
+         *  
+         *  Return appropriate status message.
+         *
+         * @return string
+         */
 
         private function getStatusMessage() {
             $status = array(
@@ -33,17 +49,32 @@
                         406 => 'Not Acceptable');
             return ($status[$this->_code]) ? $status[$this->_code] : $status[500];
         }
+
+        /* Get request method.
+         *
+         *   
+         *  @return string
+         */
         
-        public function getRequestMethod() {
+        protected function getRequestMethod() {
             return isset($_SERVER['REQUEST_METHOD']) ? 
                          $_SERVER['REQUEST_METHOD'] : null;
         }
+
+        /*  Get authorization token
+         *  
+         *  @return string
+         */
         
-        public function getAuthorization() {
+        protected function getAuthorization() {
             $headers = (array)apache_request_headers();
             return $headers["Authorization"];
         }
-        
+
+        /*  Get input according to the request method.
+         *   
+         *  @return void
+         */ 
 
         private function inputs() {
             switch($this->getRequestMethod()) {
@@ -64,6 +95,14 @@
             }
         }        
         
+        /*  Clean inputs.
+         *  
+         *
+         *  @param string
+         *
+         *  @return void
+         */
+     
         private function cleanInputs($data) {
             $clean_input = array();
             if (is_array($data)) {
@@ -79,7 +118,11 @@
             }
             return $clean_input;
         }        
-        
+        /* Set headers.
+         *  
+         * @return void
+         */
+
         private function setHeaders() {
             header("HTTP/1.1 ".$this->_code." ".$this->getStatusMessage());
             header("Content-Type:".$this->_content_type);
